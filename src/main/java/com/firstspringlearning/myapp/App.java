0db4e7469e7a35d991de5b5382105d15672a474e
Patrick Lai <patrick.lai@ringcentral.com>
2022-03-17 20:33:32 +0800

    prototype scope of bean

--- src/main/java/com/firstspringlearning/myapp/App.java
+++ src/main/java/com/firstspringlearning/myapp/App.java
@@ -4,11 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class  App {
     public static void main(String[] args) {
         ApplicationContext cxt = new ClassPathXmlApplicationContext("application-context.xml");
-        MyService bean = cxt.getBean(MyService.class);
-
-        bean.doSomething();
+        MyService bean1 = cxt.getBean(MyService.class);
+        MyService bean2 = cxt.getBean(MyService.class);
+        boolean isSameBean = bean1 == bean2; // false
+        System.out.println("bean1 is equal to bean2: " + isSameBean);
     }
 }
