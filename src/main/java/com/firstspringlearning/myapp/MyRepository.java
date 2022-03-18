cfc791bb70663f92b3946734e9fbcb5bca2f567b
Patrick Lai <patrick.lai@ringcentral.com>
2022-03-18 20:51:34 +0800

    Coding Beans to be Auto-managed and Auto-wired by Spring through Annotations

--- src/main/java/com/firstspringlearning/myapp/MyRepository.java
+++ src/main/java/com/firstspringlearning/myapp/MyRepository.java
@@ -1,5 +1,8 @@
 package com.firstspringlearning.myapp;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MyRepository {
     public void doQuery() {
         System.out.println("MyRepository.doQuery");
--- src/main/java/com/firstspringlearning/myapp/MyService.java
+++ src/main/java/com/firstspringlearning/myapp/MyService.java
@@ -1,14 +1,15 @@
 package com.firstspringlearning.myapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MyService {
 
+    @Autowired
     MyRepository repository;
 
     public void doBusinessLogic() {
         repository.doQuery();
-    }
-
-    public void setRepository(MyRepository repository) {
-        this.repository = repository;
     }
 }
