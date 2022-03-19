ba325c74138dc46b57fdab102105bd4d4d0138df
Patrick Lai <patrick.lai@ringcentral.com>
2022-03-19 10:54:52 +0800

    Setting the Current Active Profile Programmatically and Through a Property Switch

--- src/main/java/com/firstspringlearning/myapp/App.java
+++ src/main/java/com/firstspringlearning/myapp/App.java
@@ -3,12 +3,12 @@ package com.firstspringlearning.myapp;
 import com.firstspringlearning.myapp.busness.AnotherServiceImpl;
 import com.firstspringlearning.myapp.busness.MyService;
 import com.firstspringlearning.myapp.busness.MyServiceImpl;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class  App {
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "local");
         ApplicationContext cxt = new AnnotationConfigApplicationContext(AppConfig.class);
         MyService service1 = cxt.getBean(MyServiceImpl.class);
         MyService service2 = cxt.getBean(AnotherServiceImpl.class);
