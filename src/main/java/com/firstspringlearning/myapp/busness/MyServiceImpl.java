71d9a136b1b9b6737641090e1bac7ac9842401ef
Patrick Lai <patrick.lai@ringcentral.com>
2022-03-19 10:51:41 +0800

    Accessing the Current Active Profiles using the Environment Interface

--- src/main/java/com/firstspringlearning/myapp/busness/MyServiceImpl.java
+++ src/main/java/com/firstspringlearning/myapp/busness/MyServiceImpl.java
@@ -2,16 +2,20 @@ package com.firstspringlearning.myapp.busness;
 
 import com.firstspringlearning.myapp.data.MyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
-public class MyServiceImpl implements MyService {
+public class MyServiceImpl implements MyService, EnvironmentAware {
     @Value("${my.name:Patrick}")
     private String myName;
 
     private MyRepository repository;
+    private Environment environment;
 
     @Autowired
     public MyServiceImpl(MyRepository repository) {
@@ -21,6 +25,12 @@ public class MyServiceImpl implements MyService {
     @Override
     public void doBusinessLogic() {
         System.out.println("MyService.doBusinessLogic by " + myName);
+        System.out.println("Active profile: " + Arrays.toString(environment.getActiveProfiles()));
         repository.doQuery();
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
